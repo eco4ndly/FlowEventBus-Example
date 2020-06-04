90b193169a09a30959f3f7f9b08bab387693fd8e
Sayan Porya <sayanporya@gmail.com>
2020-06-04 14:22:40 +0530

    Initial commit

---
+++ app/src/main/java/com/eco4ndly/floweventbus_example/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eco4ndly.floweventbus_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
